fafdd17cc411a97fb834fd1b14d9764be4a55da9
Brian McCall <bpmccallnpm@gmail.com>
2021-09-19 11:21:03 -0700

    Use the Bond API to query the TV state and turn the TV on if necessary and if the device info has been provided.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function BondTvChannel(log, config) {
   this.log = log;
   this.name = config.name;
   this.revertTimer = null;
-  this.requestTimer = null;
+  this.powerOnTimer = null;
   this.bond = config.bond;
   this.token = config.token;
   this.freq = config.freq;
@@ -24,6 +24,11 @@ function BondTvChannel(log, config) {
   this.modulation = config.modulation;
   this.encoding = config.encoding;
   this.data = config.data;
+  this.tv_defined = config.TV ? true : false;
+  if (this.tv_defined)
+  {
+    this.TV = config.TV;
+  }
   this._service = new Service.Switch(this.name);
   
   this.cacheDirectory = HomebridgeAPI.user.persistPath();
@@ -49,9 +54,9 @@ BondTvChannel.prototype._setOn = function(on, callback) {
 
   var log = this.log;
 
-  const bondurl = "http://" + this.bond + "/v2/signal/tx";
+  const channelurl = "http://" + this.bond + "/v2/signal/tx";
 
-  var body = JSON.stringify({"freq": this.freq,
+  var channelRequestBody = JSON.stringify({"freq": this.freq,
 		"bps": this.bps,
 		"reps": this.reps,
 		"modulation": this.modulation,
@@ -67,20 +72,79 @@ BondTvChannel.prototype._setOn = function(on, callback) {
     }
   };
 
-  this.requestTimer = setTimeout(function() {
+  if (this.tv_defined)
+  {
+    const powerurl = "http://" + this.bond + "/v2/devices/" + this.TV + "/state";
+    const poweronurl = "http://" + this.bond + "/v2/devices/" + this.TV + "/actions/TurnOn";
+
+    request({method: 'GET', url: powerurl, headers: {'BOND-Token': token, 'Content-Type': 'application/json'}},
+      function(error, response, body) {
+        var power_state = 0;
+        if (error) {
+          log("Could not get power state.");
+        } else {
+          log("Response body is " + body);
+	  var body_parsed = JSON.parse(body);
+	  log('body["power"] is ' + body_parsed["power"]);
+          power_state = body_parsed["power"];
+        }
+
+        if (power_state == 0) {
+          log("Need to power on the TV.");
+
+          request({method:'PUT', url: poweronurl, headers: {'BOND-Token': token, 'Content-Type': 'application/json'}, body: '{}'},
+            function(error, response, body) {
+              if (error) {
+                log("Error turning on the TV.");
+              }
+
+              this.powerOnTimer = setTimeout(function() {
+
+                  request({method:'PUT', url: channelurl, headers: {'BOND-Token': token, 'Content-Type': 'application/json'}, body: channelRequestBody},
+                    function(error, response, body) {
+                      if (error) {
+                        log("Error changing the channel.");
+		                    log(response);
+                      }
+                    }
+                  );
+
+                }.bind(this), 10000);
+
+            }
+          );
+
+        } else {
+              log("No need to power on the TV.");
+              request({method:'PUT', url: channelurl, headers: {'BOND-Token': token, 'Content-Type': 'application/json'}, body: channelRequestBody},
+                function(error, response, body) {
+                  if (error) {
+                    log("Error changing the channel.");
+                    log(response);
+                  }
+                }
+              );
+        }
+
+      }
+    );
+
+  }
+  else
+  {
+
     request({
       method: 'PUT',
-      url: bondurl,
+      url: channelurl,
       headers: {
           'BOND-Token': token,
           'Content-Type': 'application/json'
         },
-      body: body
+      body: channelRequestBody
     },
     requestCallback
     );
-
-  }, 3000);
+  }
 
   this.revertTimer = setTimeout(function() {
     this._service.setCharacteristic(Characteristic.On, false);
